75fb4345ff2b8379df32e75c704663156416a256
Frank Marazita <fmarazita1@gmail.com>
2024-10-04 22:27:28 +1000

    added file rename

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const puppeteer = require("puppeteer");
 
-async function printPDF(url) {
+async function printPDF(url, fileName) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: "networkidle2" });
 
   const title = await page.title();
-  const fileName = title.replace(/[^a-z0-9]/gi, "_").toLowerCase() + ".pdf";
+  const titleString = title.replace(/[^a-z0-9]/gi, "_").toLowerCase();
 
-  await page.pdf({ path: fileName, format: "A4" });
+  await page.pdf({
+    path: (fileName || titleString) + ".pdf",
+    format: "A4",
+  });
 
   await browser.close();
 }
 
 const url = process.argv[2];
+const fileName = process.argv[3];
 
 if (!url) {
   console.error("Please provide a URL as an argument");
   process.exit(1);
 }
 
-printPDF(url).catch((error) => {
+printPDF(url, fileName).catch((error) => {
   console.error(error);
   process.exit(1);
 });
